d4e81e4328fadb6adf093b9c198a1b65faec3d61
Tamim Islam <nottamimislam@gmail.com>
2025-03-03 22:37:00 +0600

    Etai last 3

--- event_management/urls.py
+++ event_management/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from user import views 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,6 @@ urlpatterns = [
     path('activate/<int:uid>/<str:token>/', views.activate, name='activate')
  
 ]
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
