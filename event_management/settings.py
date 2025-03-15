4045107b1f630b6bdf207f7dad6c4a7c790b8b69
Tamim Islam <nottamimislam@gmail.com>
2025-03-15 06:08:12 +0600

    DB

--- event_management/settings.py
+++ event_management/settings.py
@@ -84,24 +84,24 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'event',
-        'USER': 'postgres',
-        'PASSWORD': 'Tamim1200',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         # Replace this value with your local database's connection string.
-#         default='postgresql://event_management_db_mkiv_user:jX9Xo1V3RxR9IJiuwuR6F86VoPKTSdQf@dpg-cuja1dogph6c73bddug0-a.oregon-postgres.render.com/event_management_db_mkiv',
-#         conn_max_age=600
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'event',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Tamim1200',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://even_db_punl_user:pBOg5sZ7NN9FukrPH1xyyS3kBGuM3wOE@dpg-cvabvgrtq21c73cc7smg-a.oregon-postgres.render.com/even_db_punl',
+        conn_max_age=600
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
